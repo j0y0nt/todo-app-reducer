a43d0bec47cebd0a3a285782987322cbb400b532
j0y0nt <4onsocial@gmail.com>
2023-03-09 09:18:09 +0530

    Add product table header.

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,8 +1,25 @@
 //import {useState} from 'react';
 
 export default function ProductList({products, onDeleteProduct}) {
+
+    const productsHeader = (
+	    <li>
+	    <div className="productContainer">
+	    <div className="productItem" style={{alignItems: 'center'}}>
+	    <div style={{width: '2%', padding: '5px'}}>No.</div>
+	    <div style={{width: '15%', textAlign: 'center'}}>Name</div>
+	    <div className="" style={{width: '40%', }}>Description</div>
+	    <div className="" style={{width: '10%', }}>Status</div>
+	    <div className="" style={{width: '10%', margin: 'auto'}}>Action</div>
+	    </div>
+	    </div>
+	    </li>
+
+    );
+    
     return (
 	    <ul className="no-bullets surface">
+	    {productsHeader}
 	    {products.map((product) => (
 		    <li key={product.id}>
 		    <Product product={product} onDeleteProduct={onDeleteProduct} />
